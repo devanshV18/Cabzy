b94698a58da454813918a54b9605adeca3b5f026
devanshV18 <devanshverma024@gmail.com>
2024-12-16 15:32:09 +0530

    basic server set up done

---
+++ server/app.js
@@ -0,0 +1,22 @@
+import { config } from "dotenv";
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+config({
+    path: "./config/config.env"
+})
+
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true
+}))
+
+app.get('/', (req,res) => {
+    res.send("<h1>Server Testing</h1>")
+})
+
+
+export default app;
