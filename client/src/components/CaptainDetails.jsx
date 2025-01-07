07fddbe858f383b264804a1f616f08895984e011
Devansh Verma <143462246+devanshV18@users.noreply.github.com>
2025-01-07 10:45:50 +0530

    starting socket.io server set up

--- client/src/components/CaptainDetails.jsx
+++ client/src/components/CaptainDetails.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { useContext } from 'react'
+import { CaptainDataContext } from '../context/CaptainContext'
 
 const CaptainDetails = () => {
+
+  const {captain} = useContext(CaptainDataContext)
+
   return (
     <div>
       <div className='flex items-center justify-between'>
               <div className='flex items-center justify-bewtween gap-3'>
                 <img className='h-10 w-10 rounded-full object-cover' src="https://media.istockphoto.com/id/1413761196/photo/happy-mature-man-driving-car.jpg?s=612x612&w=0&k=20&c=cHortB6t2CuIicx-UzOiq2jyfXufja9vETTN9dmThG4=" alt="" />
-                <h4 className='text-lg font-medium'>Harsh Upadhyay</h4>
+                <h4 className='text-lg font-medium capitalize'>{captain.fullname.firstname + " " + captain.fullname.lastname}</h4>
               </div>
               <div>
                 <h4 className='text-xl font-semibold'>₹295.20</h4>
